e96b0322ea666acb6f08ae16412f6e712d4e5d18
jaimezzapata <zapataval2304@gmail.com>
2024-04-23 08:33:35 -0500

    Se caprutan datos del formulario

---
+++ script.js
@@ -0,0 +1,5 @@
+function iniciarSesion() {
+  let usuario = document.getElementById("usuario").value;
+  let contrasena = document.getElementById("contrasena").value;
+  let correo = document.getElementById("correo").value;
+}
