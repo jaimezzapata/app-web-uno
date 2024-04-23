4b98bd52797181c672e7eaea9366e27ca483a6e6
jaimezzapata <zapataval2304@gmail.com>
2024-04-23 09:26:39 -0500

    Se implementa método push

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let usuarioNuevo = {};
+let usuarios = [];
 function iniciarSesion() {
   let usuario = document.getElementById("usuario").value;
   let contrasena = document.getElementById("contrasena").value;
   let correo = document.getElementById("correo").value;
+  usuarioNuevo = {
+    usuario,
+    contrasena,
+    correo,
+  };
+  usuarios.push(usuarioNuevo);
+  console.log(usuarios);
 }
