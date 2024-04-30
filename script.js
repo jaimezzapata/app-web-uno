6e1c4393a8ec44fe85f7f790d25e64f55dabb50d
jaimezzapata <zapataval2304@gmail.com>
2024-04-30 08:24:27 -0500

    Se crea formulario de registro y se hace funcion de cambio de fomrularios

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function iniciarSesion() {
 }
 let btnIniciar = document.getElementById("btnIniciar");
 btnIniciar.addEventListener("click", iniciarSesion);
+
+function mostrarLogin() {
+  console.log("Formulario Login");
+  document.getElementById("form-title").textContent = "Iniciar Sesión";
+  document.getElementById("register-form").style.display = "none";
+  document.getElementById("login-form").style.display = "block";
+}
+
+function mostrarRegistro() {
+  console.log("Formulario Registro");
+  document.getElementById("form-title").textContent = "Registrar Usuario";
+  document.getElementById("register-form").style.display = "block";
+  document.getElementById("login-form").style.display = "none";
+}
