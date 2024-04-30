c498c273616b427c0811d364bd4815ba91ead8e3
jaimezzapata <zapataval2304@gmail.com>
2024-04-29 22:05:29 -0500

    Se finalizan los estilos del login

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ let usuarios = [];
 function iniciarSesion() {
   let usuario = document.getElementById("usuario").value;
   let contrasena = document.getElementById("contrasena").value;
-  let correo = document.getElementById("correo").value;
   usuarioNuevo = {
     usuario,
     contrasena,
-    correo,
   };
   usuarios.push(usuarioNuevo);
   console.log(usuarios);
