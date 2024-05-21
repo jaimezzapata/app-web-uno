66dbec175f124f6255a894278cfce23566dbf3f7
jaimezzapata <zapataval2304@gmail.com>
2024-05-21 08:25:43 -0500

    Se valian todos los campos con expresiones regualres

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 import { iniciarSesion } from "../controller/ControllerUsuario.js";
-
 iniciarSesion();
 
 let inputs = document.querySelectorAll("input");
 let user = /^[A-Z]{4,10}$/;
 let password = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[\W_]).{5,10}$/;
+let correo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const validarCampos = (e) => {
   switch (e.target.name) {
@@ -12,13 +12,55 @@ const validarCampos = (e) => {
       if (user.test(e.target.value)) {
         console.log("Pasó la validación del usuario");
         document.getElementById("usuario").style.borderColor = "green";
+        document.getElementById("usuario").nextSibling.textContent = "";
       } else {
         document.getElementById("usuario").style.borderColor = "red";
+        document.getElementById("usuario").nextSibling.textContent =
+          "Campo Incorrecto";
       }
       break;
     case "contrasena":
       if (password.test(e.target.value)) {
-        console.log("Pasó la validación de contraseña");
+        document.getElementById("contrasena").style.borderColor = "green";
+        document.getElementById("contrasena").nextSibling.textContent = "";
+      } else {
+        document.getElementById("contrasena").style.borderColor = "red";
+        document.getElementById("contrasena").nextSibling.textContent =
+          "Campo Incorrecto";
+      }
+      break;
+    case "registro-usuario":
+      if (user.test(e.target.value)) {
+        document.getElementById("registro-usuario").style.borderColor = "green";
+        document.getElementById("registro-usuario").nextSibling.textContent =
+          "";
+      } else {
+        document.getElementById("registro-usuario").style.borderColor = "red";
+        document.getElementById("registro-usuario").nextSibling.textContent =
+          "Campo Incorrecto";
+      }
+      break;
+    case "registro-contrasena":
+      if (password.test(e.target.value)) {
+        document.getElementById("registro-contrasena").style.borderColor =
+          "green";
+        document.getElementById("registro-contrasena").nextSibling.textContent =
+          "";
+      } else {
+        document.getElementById("registro-contrasena").style.borderColor =
+          "red";
+        document.getElementById("registro-contrasena").nextSibling.textContent =
+          "Campo Incorrecto";
+      }
+      break;
+    case "registro-correo":
+      if (correo.test(e.target.value)) {
+        document.getElementById("registro-correo").style.borderColor = "green";
+        document.getElementById("registro-correo").nextSibling.textContent = "";
+      } else {
+        document.getElementById("registro-correo").style.borderColor = "red";
+        document.getElementById("registro-correo").nextSibling.textContent =
+          "Campo Incorrecto";
       }
       break;
   }
