9b65fcbdf195f967ebc09e647a5a6e25cc75750c
jaimezzapata <zapataval2304@gmail.com>
2024-05-28 10:16:41 -0500

    Se crean cards en el html

--- controller/ControllerProductos.js
+++ controller/ControllerProductos.js
@@ -1,20 +1,27 @@
 import productos from "../model/ModelProductos.js";
 
 export function listarProductos() {
-  let card = document.createElement("section");
-  let nombreProducto = document.createElement("h4");
-  let precioProducto = document.createElement("p");
-  let marcaProducto = document.createElement("p");
-  let descripcionProducto = document.createElement("p");
-  /* appendChild */
-  /* append */
-  card.append(
-    precioProducto,
-    marcaProducto,
-    descripcionProducto,
-    nombreProducto
-  );
-
-  console.log(card);
-  console.log(productos);
+  document.getElementById("cards").innerHTML = "";
+  productos.map((producto) => {
+    let card = document.createElement("section");
+    let nombreProducto = document.createElement("h4");
+    let precioProducto = document.createElement("p");
+    let marcaProducto = document.createElement("p");
+    let descripcionProducto = document.createElement("p");
+    nombreProducto.textContent = producto.nombre;
+    precioProducto.textContent = producto.precio;
+    marcaProducto.textContent = producto.marca;
+    descripcionProducto.textContent = producto.descripcion;
+    /* appendChild */
+    /* append */
+    card.append(
+      nombreProducto,
+      precioProducto,
+      marcaProducto,
+      descripcionProducto
+    );
+    document.getElementById("cards").appendChild(card);
+    console.log(card);
+    console.log(productos);
+  });
 }
