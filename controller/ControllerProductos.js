b255916a5e007595deb1087a83b0d179e911fdcd
jaimezzapata <zapataval2304@gmail.com>
2024-05-28 09:00:10 -0500

    Se crean elementos html desde js

--- controller/ControllerProductos.js
+++ controller/ControllerProductos.js
@@ -1,5 +1,20 @@
 import productos from "../model/ModelProductos.js";
 
 export function listarProductos() {
+  let card = document.createElement("section");
+  let nombreProducto = document.createElement("h4");
+  let precioProducto = document.createElement("p");
+  let marcaProducto = document.createElement("p");
+  let descripcionProducto = document.createElement("p");
+  /* appendChild */
+  /* append */
+  card.append(
+    precioProducto,
+    marcaProducto,
+    descripcionProducto,
+    nombreProducto
+  );
+
+  console.log(card);
   console.log(productos);
 }
